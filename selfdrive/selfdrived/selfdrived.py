7e7493869dec632c69b80c220f0363d9012817a1
PenitentTangent2401 <klabuschagne@kd9atp.com>
2025-01-23 20:21:53 -0600

    remove personality sync

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -363,11 +363,11 @@ class SelfdriveD:
         self.events.add(EventName.personalityChanged)
 
     # for SecOC cars synchronize the driving personality with the current PCM following distance
-    if self.CP.secOcRequired and self.CP.openpilotLongitudinalControl:
-      if CS.out.cruiseState.enable and (3 - CS.pcm_follow_distance != self.personality):
-        self.personality = 3 - CS.pcm_follow_distance
-        self.params.put_nonblocking('LongitudinalPersonality', str(self.personality))
-        self.events.add(EventName.personalityChanged)
+    # if self.CP.secOcRequired and self.CP.openpilotLongitudinalControl:
+    #   if CS.out.cruiseState.enable and (3 - CS.pcm_follow_distance != self.personality):
+    #     self.personality = 3 - CS.pcm_follow_distance
+    #     self.params.put_nonblocking('LongitudinalPersonality', str(self.personality))
+    #     self.events.add(EventName.personalityChanged)
 
   def data_sample(self):
     car_state = messaging.recv_one(self.car_state_sock)
